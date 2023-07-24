0f0583e481cb37f019b3973563c51d7c99442210
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2023-07-24 21:59:09 +0530

    Version 1.0

---
+++ PStorebyWhiteLotus/database.js
@@ -0,0 +1,299 @@
+const data = [
+  {
+    id: 1,
+    dp: "../fav-icon/Krishdp.jpg",
+    name: "Krishnendu Mitra",
+    //userid: "PSWL?001.kid",
+    userid: "K",
+    //password: "@Krishnendu14/02/2023",
+    password: "m",
+    social: [["krishnendumitra24@gmail.com"],["https://www.facebook.com/dibyendu.mitra.52090?mibextid=ZbWKwL"],["https://www.linkedin.com/in/krishnendu-mitra-b94004261"],["#"]],
+    type: 1,
+    time: "14/02/2023",
+    shirt: "XL",
+    alergy: 0,
+    veg: 0,
+    product: [{productDp: "../pic/daa.png",productName: "God Particle",productLink: "https://whitelotus4.github.io/weatherbyWHITELOTUS.github.io/PStorebyWhiteLotus.html#PSWL1?details=God%20particle.kid",productType: "Research Paper",productModify: "14/02/2023",productLicence: "Private-lotus"},
+      {productDp: "../pic/daa.png",productName: "New God particle",productLink: "https://whitelotus4.github.io/weatherbyWHITELOTUS.github.io/PStorebyWhiteLotus.html#PSWL1?details=Astronomy%20for%20India.kid",productType: "Research Paper",productModify: "14/02/2023",productLicence: "Publick-lotus"}
+    ],
+    quote: "Space is the study of everything in the universe that's beyond our own mind ",
+    pronouns: "He/Him",
+    text:
+      `Ahoy hoy everyone, my self Krishnendu Mitra, and I'm a freelance web Developer with extensive experience for over 5 years. My expertise is to create and website design, thumbnails design, poster making and many more. In past I am work with world's famous companies like Alphabet and Edequ. I do my job very professionaly and smartways...<br>
+      You known that i am also an admin of WHITE LOTUS, and member of WHITE LOTUS Tech support where we try to help every users who need any technical help, My few documents are avalible in Product store , try to check out.`,
+  },
+  {
+    id: 2,
+    dp: "../fav-icon/Souvikdp.png",
+    name: "Souvik Kar",
+    userid: "PSWL?002.kid",
+    password: "@Souvik14/02/2023",
+    social: [["#"],["#"],["#"],["#"]],
+    type: 1,
+    time: "14/02/2023",
+    shirt: "XL",
+    alergy: 0,
+    veg: 1,
+    product: [],
+    quote: "",
+    pronouns: "Don't specify",
+    text:
+      "",
+  },
+  {
+    id: 3,
+    dp: "../fav-icon/Rajanyadp.png",
+    name: "Rajanya Chakraborty",
+    userid: "PSWL?003.kid",
+    password: "@Rajanya14/02/2023",
+    social: [["#"],["#"],["#"],["#"]],
+    type: 1,
+    time: "14/02/2023",
+    shirt: "XL",
+    alergy: 0,
+    veg: 1,
+    product: [],
+    quote: "",
+    pronouns: "Don't specify",
+    text:
+      "",
+  },
+  {
+    id: 4,
+    dp: "../fav-icon/Hritickdp.jpg",
+    name: "Hritick Modak",
+    userid: "PSWL?004.kid",
+    password: "@Hritick14/02/2023",
+    social: [["#"],["#"],["#"],["#"]],
+    type: 1,
+    time: "14/02/2023",
+    shirt: "XL",
+    alergy: 0,
+    veg: 1,
+    product: [],
+    quote: "",
+    pronouns: "Don't specify",
+    text:
+      "",
+  },
+  {
+    id: 5,
+    dp: "../img/user(unknown).png",
+    name: "Unknown space",
+    userid: "PSWL?005.kid",
+    password: "@Unknown18/02/2023",
+    social: [["#"],["#"],["#"],["#"]],
+    type: 0,
+    time: "18/02/2023",
+    shirt: "XXL",
+    alergy: 0,
+    veg: 1,
+    product: [{productDp: "../pic/env.png",productName: "Astronomy for India",productLink: "https://whitelotus4.github.io/weatherbyWHITELOTUS.github.io/PStorebyWhiteLotus.html#PSWL1?details=Astronomy%20for%20India.kid",productType: "Presentation ppt",productModify: "18/02/2023",productLicence: "Publick-lotus"}],
+    quote: "",
+    pronouns: "Don't specify",
+    text:
+      "",
+  },
+  {
+    id: 6,
+    dp: "../img/user(unknown).png",
+    name: "CodeX",
+    userid: "PSWL?006.kid",
+    password: "@Unknown18/02/2023",
+    social: [["#"],["#"],["#"],["#"]],
+    type: 0,
+    time: "18/02/2023",
+    shirt: "XXL",
+    alergy: 0,
+    veg: 1,
+    product: [],
+    quote: "",
+    pronouns: "Don't specify",
+    text:
+      "",
+  }
+];
+let kilometres=data.length;
+const pages = [
+  {
+    id: 1,
+    total: 11,
+    hostated: 6,
+    store: 1,
+    privet: 1,
+  }
+];
+const googleForm = [
+  {
+    id: 1,
+    formname: "Register in White Lotus",
+    description: '',
+    link: "https://form.goo.gle/",
+  },
+  {
+    id: 2,
+    formname: "Type-1 Product Entrey",
+    description: '',
+    link: "https://form.goo.gle/",
+  },
+  {
+    id: 3,
+    formname: "Type-2 Product Entrey",
+    description: '',
+    link: "https://forms.gle/",
+  },
+  {
+    id: 4,
+    formname: "Edit product data",
+    description: '',
+    link: "https://forms.gle/cKwdEskWRbk8CPHV6",
+  },
+  {
+    id: 5,
+    formname: "Delete product data",
+    description: '',
+    link: "https://form.goo.gle/",
+  },
+  {
+    id: 6,
+    formname: "Edit profile information",
+    description: '',
+    link: "https://form.goo.gle/",
+  }
+];
+const Licence = [
+  {
+    id: 1,
+    licenceName: "Publick-lotus",
+    licenceBody: `<h4>Public-lotus Licence</h4>
+    <p>
+      Copyright © <span id='licence-date'>2023</span> <span id='licence-product'>WHITE LOTUS</span><br>
+      Authorities access by <span id='licence-author'>You</span>,<br>
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this product and associated documentation data (images,video,screenshot,crop pages,contents etc of this product), to deal
+      in the document without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the document, and to permit persons to whom the product is
+      furnished to do so, subject to the following conditions:<br>
+      <br>
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the product.<br>
+      Always mentioned the author name and reference of WHITE LOTUS products.<br>
+      Contact author to get permission of access this product.<br>
+      <br>
+      The product is provided "as is", without warranty of any kind, express or implied,
+      including but not limited to the warranties of merchantability,
+      fitness for a particular purpose and non-infringement. In no event shall the 
+      authors or copyright holders be liable for any claim, damages or other
+      liability, whether in an action of contract, tort or otherwise, arising from, 
+      out of or in connection with the product or the use or other dealing in this product.
+      <br>
+    </p><br>
+    <p class="null">If you want change this licence to private-lotus licence then contact us using email.</p>`,
+  },
+  {
+    id: 2,
+    licenceName: "Private-lotus",
+    licenceBody: `<h4>Private-lotus Licence</h4>
+    <p>
+      Copyright © <span id='licence-date'>2023</span> <span id='licence-product'>WHITE LOTUS</span><br>
+      Authorities access by <span id='licence-author'>You</span>,<br>
+      Permission is hereby granted, minimum of charge, to any person obtaining a copy
+      of this product and associated documentation data (images,video,screenshot,crop pages,contents etc of this product), to deal
+      in the document with some restriction, including few limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, (but never sell
+      copies of this document) and to permit persons to whom the product is
+      furnished to do so, subject to the following conditions:<br>
+      <br>
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the product.<br>
+      Always mentioned the original author name and reference of WHITE LOTUS products.<br>
+      Contact author to get permission of access this product.<br>
+      <br>
+      The product is provided "as is", without warranty of any kind, express or implied,
+      including but not limited to the warranties of merchantability,
+      fitness for a particular purpose and non-infringement. In no event shall the 
+      authors or copyright holders be liable for any claim, damages or other
+      liability, whether in an action of contract, tort or otherwise, arising from, 
+      out of or in connection with the product or the use or other dealing in this product.
+      <br>
+    </p><br>
+    <p class="null">If you want change this licence to publick-lotus licence then contact us using email.</p>`,
+  },
+];
+var col1 = ['aqua', 'lightblue', '#0c8ff0', 'blue', '#9834FF', '#00E3FF', '#8CABFF', '#0A0073'];
+const set1 = ['Ebook','Report Paper','Biography/Autography','Notes','Presentation ppt','Book pdf','Research Paper','Question Paper'];
+//mute
+const set2 = ['Ebook','Report Paper','Biography/Autography','Notes','Presentation ppt','Book pdf','Research Paper','Question Paper'];
+const blockChain = [
+  {
+    id: 1,
+    Date: '13/02/2023',
+    Form: 'Souvik,Rajanya,Hritic,Krishnendu',
+    To: 'Green Tech Fest',
+    Cause: 'Join to devlope a website and get certificate',
+    Amount: 0,
+    Method: 'Virtual',
+    Stack: 0
+  },
+  {
+    id: 2,
+    Date: '14/02/2023',
+    Form: 'White Lotus',
+    To: 'Krishnendu Mitra',
+    Cause: 'Devlope White Lotus',
+    Amount: 0,
+    Method: 'Virtual',
+    Stack: 0
+  },
+  {
+    id: 3,
+    Date: '18/02/2023',
+    Form: 'Krishnendu Mitra',
+    To: 'INSAT-3DR',
+    Cause: 'Access weather data',
+    Amount: 0,
+    Method: 'Virtual',
+    Stack: 0
+  },
+  {
+    id: 4,
+    Date: '18/02/2023',
+    Form: 'Krishnendu Mitra',
+    To: 'GSAT-6',
+    Cause: 'Access weather data',
+    Amount: 0,
+    Method: 'Virtual',
+    Stack: 0
+  },
+  {
+    id: 5,
+    Date: '18/02/2023',
+    Form: 'Krishnendu Mitra',
+    To: 'NOAA-M',
+    Cause: 'Access weather data',
+    Amount: 0,
+    Method: 'Virtual',
+    Stack: 0
+  },
+  {
+    id: 6,
+    Date: '02/03/2023',
+    Form: 'Krishnendu Mitra',
+    To: 'Github',
+    Cause: 'Host White Lotus Website in github',
+    Amount: 0,
+    Method: 'Virtual',
+    Stack: 0
+  },
+  {
+    id: 7,
+    Date: '09/03/2023',
+    Form: 'White Lotus',
+    To: 'Krishnendu Mitra',
+    Cause: 'Youtube video',
+    Amount: 0,
+    Method: 'Virtual',
+    Stack: 0
+  }
+];
---
+++ PStorebyWhiteLotus/productbase.js
@@ -0,0 +1,123 @@
+const product = [
+  {
+    id: 1,
+    head: "God Particle",
+    twoline: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero aut delectus omnis, earum repudiandae quam alias enim illo vel nulla qui soluta esse quibusdam, quis eligendi, officia placeat ab corrupti voluptate. Iure labore debitis ullam dicta error libero, illo ducimus cum obcaecati ab corporis, perspiciatis non aspernatur quidem corrupti exercitationem atque incidunt nobis veniam quo nulla sed! Exercitationem tempore laudantium quo similique asperiores ipsa obcaecati beatae modi delectus hic impedit esse quaerat, iure eius a sequi repellat quos fugit! Quisquam similique, quo atque ipsam ex officiis perspiciatis repudiandae blanditiis quos ut tenetur quis vel, esse veritatis magni! Amet libero, a ratione aut culpa officiis modi, quis enim laudantium atque eligendi quos perspiciatis. Ipsam veniam laboriosam, debitis dolores, placeat, sit eligendi ab error natus cupiditate ducimus. Unde, fugit optio.",
+    img1: "./fav-icon/effect001.jpg",
+    img2: "./fav-icon/effect002.jpg",
+    img3: "./fav-icon/effect004.jpg",
+    link: "https://google.com/",
+    avglist: [0,0,1,2,0,10,15],
+    aboutlist: ["pdf", 10, "20kb", "All type", "English", 0, "Yes", "Yes", "Yes"],
+    ytlink: "https://www.youtube.com/embed/M63IdwIymiY?controls=0",
+    morepro: [["./pic/daa.png", "Gods Particle part 2", "https://drive.google.com/uc?export=download&id=1Csh6Gcqd-e4LTxR7U18vv9n7mG3HkTu_","20kb","pdf","Research Paper","16/02/2023"], ["./fav-icon/effect002.jpg", "Gods Particle part 3", "https://drive.google.com/uc?export=download&id=1jyBoxo0bWzmdgK-r0PJVRdF3iq35bxt7","50kb","pdf","Research Paper","05/04/2023"]],
+    text:
+      `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus est aut sint nisi magni adipisci quas facere quibusdam numquam perspiciatis veniam eligendi, dolorum ex, quod laboriosam harum repellendus laborum! Quidem natus soluta voluptatibus a autem dolores optio culpa iure voluptatum sequi libero, quae, provident vitae at beatae! Nulla totam modi delectus at, nisi quisquam dignissimos doloremque libero. Sit fugit, et dolore aspernatur accusantium enim saepe veritatis vel reiciendis dignissimos, rerum ut dicta incidunt esse quaerat hic eveniet impedit. Aspernatur magni tempora nisi maiores, impedit laudantium itaque alias obcaecati qui ullam illum libero quis id expedita eos? Repellat rerum perspiciatis veritatis velit. Error, itaque atque numquam maiores sint, provident impedit magnam sequi alias quae officia obcaecati non! Tenetur id consequuntur, reprehenderit laborum excepturi quod culpa quaerat omnis minus exercitationem!
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus est aut sint nisi magni adipisci quas facere quibusdam numquam perspiciatis veniam eligendi, dolorum ex, quod laboriosam harum repellendus laborum! Quidem natus soluta voluptatibus a autem dolores optio culpa iure voluptatum sequi libero, quae, provident vitae at beatae! Nulla totam modi delectus at, nisi quisquam dignissimos doloremque libero. Sit fugit, et dolore aspernatur accusantium enim saepe veritatis vel reiciendis dignissimos, rerum ut dicta incidunt esse quaerat hic eveniet impedit. Aspernatur magni tempora nisi maiores, impedit laudantium itaque alias obcaecati qui ullam illum libero quis id expedita eos? Repellat rerum perspiciatis veritatis velit. Error, itaque atque numquam maiores sint, provident impedit magnam sequi alias quae officia obcaecati non! Tenetur id consequuntur, reprehenderit laborum excepturi quod culpa quaerat omnis minus exercitationem!<br>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus est aut sint nisi magni adipisci quas facere quibusdam numquam perspiciatis veniam eligendi, dolorum ex, quod laboriosam harum repellendus laborum! Quidem natus soluta voluptatibus a autem dolores optio culpa iure voluptatum sequi libero, quae, provident vitae at beatae! Nulla totam modi delectus at, nisi quisquam dignissimos doloremque libero. Sit fugit, et dolore aspernatur accusantium enim saepe veritatis vel reiciendis dignissimos, rerum ut dicta incidunt esse quaerat hic eveniet impedit. Aspernatur magni tempora nisi maiores, impedit laudantium itaque alias obcaecati qui ullam illum libero quis id expedita eos? Repellat rerum perspiciatis veritatis velit. Error, itaque atque numquam maiores sint, provident impedit magnam sequi alias quae officia obcaecati non! Tenetur id consequuntur, reprehenderit laborum excepturi quod culpa quaerat omnis minus exercitationem!<br>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus est aut sint nisi magni adipisci quas facere quibusdam numquam perspiciatis veniam eligendi, dolorum ex, quod laboriosam harum repellendus laborum! Quidem natus soluta voluptatibus a autem dolores optio culpa iure voluptatum sequi libero, quae, provident vitae at beatae! Nulla totam modi delectus at, nisi quisquam dignissimos doloremque libero. Sit fugit, et dolore aspernatur accusantium enim saepe veritatis vel reiciendis dignissimos, rerum ut dicta incidunt esse quaerat hic eveniet impedit. Aspernatur magni tempora nisi maiores, impedit laudantium itaque alias obcaecati qui ullam illum libero quis id expedita eos? Repellat rerum perspiciatis veritatis velit. Error, itaque atque numquam maiores sint, provident impedit magnam sequi alias quae officia obcaecati non! Tenetur id consequuntur, reprehenderit laborum excepturi quod culpa quaerat omnis minus exercitationem!`
+  },
+  {
+    id: 2,
+    head: "Astronomy for India",
+    twoline: "Astronomy is a easy subject but not in india",
+    img1: "./fav-icon/effect0025.jpg",
+    img2: "./fav-icon/effect0035.jpg",
+    img3: "./fav-icon/effect002.jpg",
+    link: "https://youtube.com/",
+    avglist: [0,0,7,1,2,1,9],
+    aboutlist: ["ppt", 18, "50kb", "Students", "English", 0, "No", "Yes", "Yes"],
+    ytlink: "https://www.youtube.com/embed/?controls=0",
+    morepro: [["./img/product(empty).png", "Astronomy part 2", "https://drive.google.com/uc?export=download&id=1CY0OftPkAy95pimj_qiwoD_e0FiBJgjd","10kb","ppt","Presentation ppt","18/02/2023"]],
+    text:
+      "Hello, I am a Youtuber. I think you all know me yes I am King Gamer and I love White Lotus..."
+  },
+  {
+    id: 3,
+    head: "New God particle",
+    twoline: "Just for checking it",
+    img1: "./fav-icon/effect005.jpg",
+    img2: "./fav-icon/effect001.jpg",
+    img3: "./fav-icon/effect0025.jpg",
+    link: "https://facebook.com/",
+    avglist: [],
+    aboutlist: ["docx", 1, "400kb", "All type", "English", 0, "No", "Yes", "No"],
+    ytlink: "https://www.youtube.com/embed/?controls=0",
+    morepro: [],
+    text:
+      "No despo please."
+  }
+];
+let milimeter = product.length;
+//use pswl id to access productTuple element
+let productTuple=[];
+if(milimeter % 10 == 0){
+  var divisor = milimeter / 10,num1=0;
+  for(let i=0; i<divisor; i++){
+    num2 = num1+9;
+    productTuple[i] = {start: num1, end: num2};
+    num1=num2+1;
+  }
+}else{
+	var divisor = Math.floor(milimeter / 10), reminder = milimeter % 10;
+	for(let i=0; i<divisor; i++){
+        num2 = num1+9;
+        productTuple[i] = {start: num1, end: num2};
+        num1=num2+1;
+    }
+    if(productTuple[productTuple.length-1]!=undefined){
+         productTuple[productTuple.length] = {start: productTuple[productTuple.length-1]+1, end: reminder};
+    }else{
+    	productTuple[productTuple.length] = {start: 0, end: reminder};
+    }
+}
+function rateChecker(arr){
+  let T=0,M=0;
+  for(let i=2; i<7; i++){
+    T+=arr[i];
+  }
+  for(let i=2; i<7; i++){
+    M+=(arr[i]*(i-1));
+  }
+  arr[1]=T;
+  arr[0]=(M/T).toFixed(1);
+  return arr;
+}
+function rateToStarMaker(arr){
+  let num=arr*10, counter, newnum, j,p=0.1;
+    if(num % 10 != 0){
+    	num = ((num/10).toFixed(1))*1;
+        for(j=1; j<10; j++){
+            newnum = num+p;
+            if((newnum*10) % 10 == 0){
+                counter=p;
+                num=Math.floor(num);
+                break;
+            }
+            p+=0.1;
+        }
+    }else{
+        counter=1;
+        num = ((num/10).toFixed(1))*1;
+    }
+    num=num*1;
+    counter=((1-counter.toFixed(1)*1).toFixed(1)*1);
+    p='';
+    for(j=0; j<num; j++){
+    	p+=`<i class="fa fa-star checked"></i>`;
+    }
+    if(counter<=0.3 && counter>0){
+    	p+=`<i class="fa fa-star-o checked"></i>`;
+        newnum=1;
+    }else if(counter<=0.9 && counter>0.3){
+    	p+=`<i class="fa fa-star-half-o checked"></i>`;
+        newnum=1;
+    }else{
+    	p+='';
+        newnum=0;
+    }
+    for(j=0; j<(5-(num+newnum)); j++){
+    	p+=`<i class="fa fa-star"></i>`;
+    }
+    return p;
+}
