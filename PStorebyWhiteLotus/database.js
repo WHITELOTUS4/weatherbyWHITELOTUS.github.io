f482aba72c7c4c233f0717492fdf0f8853fe3bb6
WHITELOTUS <126754757+WHITELOTUS4@users.noreply.github.com>
2025-04-29 04:48:03 -0700

    New Update

--- PStorebyWhiteLotus/database.js
+++ PStorebyWhiteLotus/database.js
@@ -37,9 +37,9 @@ const data = [
     shirt: "XL",
     alergy: 0,
     veg: 1,
-    product: [{productDp: "./pic/AI_Job.png",productName: "The Truth About AI and Job Replacement Facts and Rumour",productLink: "https://whitelotus4.github.io/weatherbyWHITELOTUS.github.io/PStorebyWhiteLotus.html#PSWL1?details=The%20Truth%20About%20AI%20and%20Job%20Replacement%20Facts%20and%20Rumour.kid",productType: "Research Paper",productModify: "29/04/2025",productLicence: "Publick-lotus",productISBN: ""}],
+    product: [{productDp: "./pic/AI_Job.jpeg",productName: "The Truth About AI and Job Replacement Facts and Rumour",productLink: "https://whitelotus4.github.io/weatherbyWHITELOTUS.github.io/PStorebyWhiteLotus.html#PSWL1?details=The%20Truth%20About%20AI%20and%20Job%20Replacement%20Facts%20and%20Rumour.kid",productType: "Research Paper",productModify: "29/04/2025",productLicence: "Publick-lotus",productISBN: ""}],
     quote: "Money looks better in the bank than on your feet. But much much hot in my pocket, so give your money to me",
-    pronouns: "Don't specify",
+    pronouns: "He/Him",
     text:
       "",
   },
